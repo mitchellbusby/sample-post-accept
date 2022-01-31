f22a8e11ba09d33b8afa12444ddd4219132b934a
Mitchell Busby <mitchbusby@gmail.com>
2022-01-31 12:19:12 +1100

    remove page title

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,13 @@
 const App = () => {
   const { search } = location;
   const searchParams = new URLSearchParams(search);
-  const pageTitle = searchParams.get("pageTitle") || "Post-accept sample";
+  const pageTitle = searchParams.get("surveyName") || "Post-acceptance survey";
 
   return (
     <div className="flex justify-center pv5">
     <div className="mw9 w-two-thirds">
       <div className="f1">{pageTitle}</div>
-      <div className="f2 mb4">You've been redirected to this page!</div>
-      <div className="f3 mb2">Supplied parameters</div>
-      <div className="f4">Page ID: {searchParams.get("pageId")}</div>
-      <div className="f4">Page title: {searchParams.get("pageTitle")} </div>
+      <div className="f2 mb4">You've been redirected to this page to do a survey for page {searchParams.get("pageId")}!</div>
     </div>
     </div>
   );
